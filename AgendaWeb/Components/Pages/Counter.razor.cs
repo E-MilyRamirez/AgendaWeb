35e45ff46d3b0bd7972bff36038e0ef9ad822fea
ramir <emilyram006@gmail.com>
2026-02-16 07:56:54 -0700

    Agregar archivos de proyecto.

---
+++ AgendaWeb/Components/Pages/Counter.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace AgendaWeb.Components.Pages
+{
+    public partial class Counter
+    {
+    private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+
+            string connectionString = "Server=localhost;Database=AgendaDb;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            String query = "INSERT INTO Contactos(Nombre, Telefono, Email) VALUES('Desde VS', '6621456823', 'Naura@gmail.com')";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.CommandType = CommandType.Text;
+
+            connection.Open();
+
+            command.ExecuteNonQuery();
+
+            connection.Dispose();
+        }
+    }
+}
---
+++ AgendaWeb/Program.cs
@@ -0,0 +1,41 @@
+using AgendaWeb.Components;
+
+namespace AgendaWeb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+            //Builder es un patron de diseño
+            //connectionString va después del CreateBuilder y antes del build, porque es parte de la configuración que se va a usar en la aplicación, y se obtiene de la configuración del builder.
+
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+            app.UseHttpsRedirection();
+
+            app.UseAntiforgery();
+
+            app.MapStaticAssets();
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
